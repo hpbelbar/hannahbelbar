ff9a443040e95d2763349358697105f8ada43d13
Hannah Belbar <152470042+hpbelbar@users.noreply.github.com>
2024-01-19 23:22:33 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Student", "Future IT Designer", "Future Business Woman"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
