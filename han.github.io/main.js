dcc76a4e4fe00f76faacf57ec142a3e07c575c23
Hannah Belbar <152470042+hpbelbar@users.noreply.github.com>
2024-01-19 23:21:39 +0800

    Delete han.github.io directory

--- han.github.io/main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["Student", "Future IT Designer", "Future Business Woman"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
